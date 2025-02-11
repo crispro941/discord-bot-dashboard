98943182226265d04c81d97fc398a2b3d2e679e0
CRISTOBAL PAEZ <81827654+crispro941@users.noreply.github.com>
2025-02-11 04:56:44 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -28,8 +28,8 @@ export const config = {
         "auto_kill_kane": KillKane
     },
     tutorialUrl: "https://github.com/SonMooSans/discord-bot-dashboard",
-    serverUrl: "http://localhost:8080",
-    inviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1004280473956139038&permissions=8&scope=bot",
+    serverUrl: "https://discord-bot-dashboard-ashy.vercel.app/",
+    inviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1053026144930238484&permissions=8&scope=bot",
     data: {
         dashboard: dashboardData,
     },
